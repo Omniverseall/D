ed3fd986d89af655c41b3f96bbe9773b46cf1fae
MrAI77 <omniverseall4215062000@gmail.com>
2025-03-19 16:10:14 +0500

    w

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,21 @@
+import { createContext } from "react";
+import PropTypes from 'prop-types';
+import { doctors } from "../assets/assets";
+export const AppContext = createContext();
+
+
+const AppContextProvider = ({children}) => {
+
+    const value = {
+      doctors,
+    }
+
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+AppContextProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default AppContextProvider;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from '../component/Header'
+import SpecialityMenu from '../component/SpecialityMenu'
+import TopDoctors from '../component/TopDoctors'
+import Banner from '../component/Banner'
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <SpecialityMenu />
+      <TopDoctors />
+      <Banner />
+    </div>
+  )
+}
+
+export default Home
